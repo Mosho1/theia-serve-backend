a0d759d1a84e081610b0b1952ee5ea6c202f911e
Sharon Rolel <srolel@uwaterloo.ca>
2018-12-04 23:19:41 -0800

    a little refactor

--- src/theia-serve-plugin-backend.ts
+++ src/theia-serve-plugin-backend.ts
@@ -7,31 +7,33 @@ import * as theia from '@theia/plugin';
 import { StaticServer } from './server';
 
 const server = new StaticServer({ port: 4000 });
-
-export function start(context: theia.PluginContext) {
-    const runServerCommand = {
-        id: 'theia-serve-run-server',
-        label: "Serve workspace"
-    };
-    const startServer = async (path: string) => {
-        try {
-            if (!theia.workspace.workspaceFolders || theia.workspace.workspaceFolders.length === 0) {
-                throw new Error('No active workspace!');
-            }
-            const path = theia.workspace.workspaceFolders[0].uri.path;
-            await server.start(path);
-            const message = `Started workspace server serving ${path} on port ${server.options.port}`;
-            theia.window.showInformationMessage(message);
-            console.log(message);
-        } catch (e) {
-            theia.window.showErrorMessage(e.message)
-            console.error(e);
+const runServerCommand = {
+    id: 'theia-serve-run-server',
+    label: "Serve workspace"
+};
+const info = (message: string) => {
+    theia.window.showInformationMessage(message);
+    console.log(message);
+};
+const error = (e: Error) => {
+    theia.window.showErrorMessage(e.message)
+    console.error(e);
+};
+const runServer = async () => {
+    try {
+        if (!theia.workspace.workspaceFolders || theia.workspace.workspaceFolders.length === 0) {
+            throw new Error('No active workspace!');
         }
-
-    };
-    theia.commands.registerCommand(runServerCommand, (...args: any[]) => {
-        startServer('/');
-    });
+        const path = theia.workspace.workspaceFolders[0].uri.path;
+        await server.start(path);
+        const message = `Started workspace server serving ${path} on port ${server.options.port}`;
+        info(message);
+    } catch (e) {
+        error(e);
+    }
+};
+export function start(context: theia.PluginContext) {
+    theia.commands.registerCommand(runServerCommand, runServer);
 }
 
 export function stop() {
